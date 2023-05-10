918799d41531c03e11ae6d04957656094a9d34c6
ronny <ronnychamba96@gmail.com>
2023-05-09 20:48:14 -0500

    disign login

---
+++ app/src/main/java/com/app/planificaciones/MainActivity.java
@@ -0,0 +1,64 @@
+package com.app.planificaciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.app.planificaciones.models.AuthCredential;
+
+import java.sql.Struct;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+
+    private Button btnSigIn;
+    private EditText txtEmail;
+    private EditText txtPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findWidgets();
+        setListener();
+    }
+
+    private void findWidgets() {
+        btnSigIn = findViewById(R.id.btnSigIn);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+    }
+
+    private void setListener() {
+        btnSigIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int idEvent = v.getId();
+        if (idEvent == btnSigIn.getId()){
+            authUser();
+        }
+    }
+    private void authUser(){
+
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if (email.isBlank() || password.isBlank()) {
+            Toast.makeText(this, "Todos los campos son requeridos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AuthCredential auth = new AuthCredential(email, password);
+
+        Toast.makeText(this, auth.toString(), Toast.LENGTH_SHORT).show();
+
+
+    }
+}
